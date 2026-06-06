91d7c9518d1e0cb6c94fecdbe597bfb9da2256b7
Nhac-Phu-Dai-Nhan <hellobuianhcuong@gmail.com>
2026-06-06 22:26:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt update && apt install -y nginx certbot python3-certbot-nginx python3 python3-venv
-curl -s https://raw.githubusercontent.com/Nhac-Phu-Dai-Nhan/upload-api-direct-link/main/setup.sh -o /tmp/setup.sh && chmod +x /tmp/setup.sh && bash /tmp/setup.sh
+apt update && apt install -y nginx certbot python3-certbot-nginx python3 python3-venv python3.12-venv
+curl -s https://raw.githubusercontent.com/Nhac-Phu-Dai-Nhan/upload-api-direct-link/main/setup.sh -o /tmp/setup.sh && bash /tmp/setup.sh
